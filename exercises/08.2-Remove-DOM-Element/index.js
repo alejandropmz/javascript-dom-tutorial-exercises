12d595ccea1035faa1336f02faa5ee2819bf67a0
alejandropmz <alejandropacheco2497@gmail.com>
2022-11-09 01:39:36 +0000

    add exercises

---
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -0,0 +1,3 @@
+let listChild = document.querySelector("#parentLi");
+
+listChild.removeChild(listChild.childNodes[3])
--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,12 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+
+	let HelloWorld = document.createElement("div");
+	HelloWorld.style.background = "yellow";
+	HelloWorld.innerHTML = "Hello World";
 	
+	document.body.appendChild(HelloWorld)
+
 });
+
